a657029be9966249eff3dfa076ed45709615aec2
tarik-gadoumi <92150292+tarik-gadoumi@users.noreply.github.com>
2022-03-09 17:41:41 +0100

    Update Request.php

--- Request.php
+++ Request.php
@@ -47,12 +47,12 @@ class Request
         $data = [];
         if ($this->isGet()) {
             foreach ($_GET as $key => $value) {
-                $data[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+                $data[$key] = filter_input(INPUT_GET, $value, FILTER_SANITIZE_SPECIAL_CHARS);
             }
         }
         if ($this->isPost()) {
             foreach ($_POST as $key => $value) {
-                $data[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+                $data[$key] = filter_input(INPUT_POST, $value, FILTER_SANITIZE_SPECIAL_CHARS);
             }
         }
         return $data;
